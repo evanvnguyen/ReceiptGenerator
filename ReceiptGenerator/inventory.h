da5f87e73954a96bedd55fcc6e52a65fd21bc31b
Evan Nguyen <59784844+evanvnguyen@users.noreply.github.com>
2022-04-28 22:09:01 -0700

    Add files via upload

--- ReceiptGenerator/inventory.h
+++ ReceiptGenerator/inventory.h
@@ -1,7 +1,15 @@
 #pragma once
+#ifndef INVENTORY_H
+#define INVENTORY_H
 //class to hold all the items of a store, one instance will be created in the ReceiptGenerator.cpp file to be
 //the inventory of a particular runtime
-class inventory
-{
+
+#include "item.h"
+class inventory {
+	private:
+		// initialize the inventory here
+		item milk(0001, "Horizon Milk 16oz", 50, 3.99);
+	public:
 };
 
+#endif
